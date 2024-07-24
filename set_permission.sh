d9a14200a8f0c6190139e4d8b6c39b367c58edc4
motii <108280115+motii8128@users.noreply.github.com>
2024-07-24 21:45:12 +0900

    Delete set_permission.sh

--- set_permission.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eu
-
-cd /etc/udev/rules.d
-
-sudo touch ./99-hidraw-permissions.rules
-
-sudo echo "KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="plugdev"" > ./99-hidraw-permissions.rules
-
