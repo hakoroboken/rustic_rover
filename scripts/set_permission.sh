f06ffd8917e49754931628623eb78bd27c99b462
motii <108280115+motii8128@users.noreply.github.com>
2024-07-24 21:45:39 +0900

    Add files via upload

---
+++ scripts/set_permission.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eu
+
+cd /etc/udev/rules.d
+
+sudo touch ./99-hidraw-permissions.rules
+
+sudo echo "KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="plugdev"" > ./99-hidraw-permissions.rules
+
