3e0d8fdaf371222ae57d57bdf338fe54f949e1a5
motii8128 <default270robo@gmail.com>
2024-08-05 10:04:10 +0900

    Fix Update

--- src/rr_core.rs
+++ src/rr_core.rs
@@ -169,31 +169,7 @@ impl iced::Application for RusticRover {
 
                 self.status.serial_state = AppState::OK;
                 
-                std::thread::spawn(move ||{
-                    let mut port_ = serialport::new(port_name_.clone().as_str(), 115200)
-                        .data_bits(serialport::DataBits::Eight)
-                        .stop_bits(serialport::StopBits::One)
-                        .timeout(std::time::Duration::from_millis(100))
-                        .open().unwrap();
-                    loop {
-                        let p = con_p.subscriber.recv().unwrap();
-                        
-                        let write_buf = format!("s{},{},{},{},{}e", 
-                                p.x/10 as i32+10,
-                                p.y/10 as i32+10,
-                                p.ro/10 as i32+10,
-                                p.m1/10 as i32+10,
-                                p.m2/10 as i32+10);
-
-                        match port_.write(write_buf.as_bytes()) {
-                            Ok(_)=>{
-                            }
-                            Err(_)=>{
-
-                            }
-                        }
-                    }
-                });
+                std::thread::spawn(move || serial::task(port_name_, con_p.subscriber));
             }
         }
 
--- src/rr_core/serial.rs
+++ src/rr_core/serial.rs
@@ -1,3 +1,5 @@
+use std::sync::mpsc::Receiver;
+
 use crate::rr_core::interface::Packet;
 use crate::rr_core::thread_connection::ThreadConnector;
 
@@ -11,4 +13,32 @@ impl SerialManager {
     {
         SerialManager { conn: ThreadConnector::<Packet>::new()}
     }
+}
+
+pub fn task(port_name_:String, packet_subscriber:Receiver<Packet>)
+{
+    let mut port_ = serialport::new(port_name_.clone().as_str(), 115200)
+            .data_bits(serialport::DataBits::Eight)
+            .stop_bits(serialport::StopBits::One)
+            .timeout(std::time::Duration::from_millis(100))
+            .open().unwrap();
+        loop {
+            let p = packet_subscriber.recv().unwrap();
+            
+            let write_buf = format!("s{},{},{},{},{}e", 
+                    p.x/10 as i32+10,
+                    p.y/10 as i32+10,
+                    p.ro/10 as i32+10,
+                    p.m1/10 as i32+10,
+                    p.m2/10 as i32+10);
+
+            match port_.write(write_buf.as_bytes()) {
+                Ok(_)=>{
+
+                }
+                Err(_)=>{
+
+                }
+            }
+        }
 }
